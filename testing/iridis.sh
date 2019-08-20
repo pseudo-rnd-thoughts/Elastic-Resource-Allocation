26efd1d3d2058983cee14ea15e217db37e638dd7
StringTheory <mark.m.towers@gmail.com>
2019-08-20 23:17:39 +0100

    Iridis Script

--- testing/iridis.sh
+++ testing/iridis.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 # Example Bash Script to execute <executable_name> on batch
 
+#PBS –l walltime=16:00:00
+#PBS –l nodes=1:ppn=16
+#PBS -m ae -M mt5g17@soton.ac.uk
+
 #Change to the directory that the job was submitted from
-cd $PBS_O_WORKDIR
-echo pwd
+cd /lyceum/mt5g17/cloud_allocation/testing
+
 #Run <executable_name> from the working directory
-./iridis.sh
+./run_iridis.sh
