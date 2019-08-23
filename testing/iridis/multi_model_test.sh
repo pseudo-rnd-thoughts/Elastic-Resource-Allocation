c59b81488e7aab707aa51eaa3bee4ed137ae56cd
StringTheory <mark.m.towers@gmail.com>
2019-08-23 18:58:28 +0100

    Fixed multi model test

--- testing/iridis/multi_model_test.sh
+++ testing/iridis/multi_model_test.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-args=( "$@" )
+args=$@
 script=${args[0]}
 
-if [ ${#args} == 1 ]; then
+if [ ${#args[@]} == 1 ]; then
   args=("" 12 2 15 3 25 5 100 20 150 25)
 fi
 
-for (( pos = 1; pos < ${#args}; pos+=2 )); do
-    qsub -v file="$script" num_jobs="${args[i]}" num_servers="${args[i+1]}" run_file.sh
+for (( pos = 1; pos < ${#args[@]}; pos+=2 )); do
+    cmd="qsub -v file=$script num_jobs=${args[i]} num_servers=${args[i+1]} run_file.sh"
 done
