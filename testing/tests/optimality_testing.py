617e3e98ba60aade135e7e92026000bd74755d9b
StringTheory <mark.m.towers@gmail.com>
2019-10-08 16:50:19 +0100

    Added optimality testing

--- testing/tests/optimality_testing.py
+++ testing/tests/optimality_testing.py
@@ -2,13 +2,31 @@
 
 from __future__ import annotations
 
-from core.model import ModelDist
+from core.core import load_args
+from core.model import ModelDist, load_dist
+from optimal.optimal import optimal_algorithm
 
 
-def optimality_testing(model_dist: ModelDist, repeat: int):
-    for time_limit in []:
-        pass
+def optimality_testing(model_dist: ModelDist):
+    """
+    Optimality testing
+    :param model_dist: The model distribution
+    :param repeat: The repeat
+    """
+    jobs, servers = model_dist.create()
+    for time_limit in [10, 30, 60, 5*60, 15*60, 60*60, 24*60*60]:
+        print("Time Limit: {}".format(time_limit))
+        result = optimal_algorithm(jobs, servers, time_limit)
+        print(result.store())
+        if result.data['solve_status'] == 'Optimal':
+            print("Solved completely at time limit: {}".format(time_limit))
+            break
 
 
 if __name__ == "__main__":
-    pass
+    args = load_args()
+
+    model_name, job_dist, server_dist = load_dist(args['model'])
+    loaded_model_dist = ModelDist(model_name, job_dist, args['jobs'], server_dist, args['servers'])
+
+    optimality_testing(loaded_model_dist)
