54ba63363ba1929a06b418cda405cc996179b4b8
StringTheory <mark.m.towers@gmail.com>
2019-10-08 13:36:52 +0100

    Added Optimality Testing

---
+++ testing/tests/optimality_testing.py
@@ -0,0 +1,14 @@
+"""Optimality Test"""
+
+from __future__ import annotations
+
+from core.model import ModelDist
+
+
+def optimality_testing(model_dist: ModelDist, repeat: int):
+    for time_limit in []:
+        pass
+
+
+if __name__ == "__main__":
+    pass
