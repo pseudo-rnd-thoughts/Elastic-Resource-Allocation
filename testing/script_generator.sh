e8c29c847b5cda67187470a3b9d5a7eafe297a5e
StringTheory <mark.m.towers@gmail.com>
2019-11-01 14:51:59 +0000

    Update script_generator.sh

--- testing/script_generator.sh
+++ testing/script_generator.sh
@@ -1,11 +1,30 @@
 #!/usr/bin/env bash
 
-read -p 'Script: ' script
-read -p 'Model: ' model
-read -p 'Num of repeats: ' repeats
-
+scripts=()
+models=()
 jobs=()
 servers=()
+
+while true; do
+  read -p 'Script: ' script
+  if [ "$script" == "" ]; then
+    break
+  else
+    scripts+=("$script")
+  fi
+done
+
+while true; do
+  read -p 'Model: ' model
+  if [ "$model" == "" ]; then
+    break
+  else
+    models+=("$model")
+  fi
+done
+
+read -p 'Num of repeats: ' repeats
+
 while true; do
   read -p 'Num of jobs: ' num_jobs
   if [ "$num_jobs" == "" ]; then
@@ -17,9 +36,13 @@ while true; do
   servers+=("$num_servers")
 done
 
-for (( pos = 0; pos < ${#jobs[@]}; pos++ )); do
-  for (( repeat = 0; repeat < repeats; repeat++ )); do
-    cmd="qsub -v file='$script'_testing,num_jobs='${jobs[pos]}',num_servers='${servers[pos]}',model='$model',repeat='$repeat' run_script.sh"
-    eval "$cmd"
+for script in $(scripts); do
+  for model in $(models); do
+    for (( pos = 0; pos < ${#jobs[@]}; pos++ )); do
+      for (( repeat = 0; repeat < repeats; repeat++ )); do
+        cmd="qsub -v file='$script'_testing,num_jobs='${jobs[pos]}',num_servers='${servers[pos]}',model='$model',repeat='$repeat' run_script.sh"
+        eval "$cmd"
+      done
+    done
   done
 done
