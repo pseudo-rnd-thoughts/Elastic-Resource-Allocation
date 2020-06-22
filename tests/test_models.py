b75647529230e3788d2fb1c5b2905c219e967b8a
StringTheory <mark.m.towers@gmail.com>
2020-06-22 14:26:51 +0100

    Updated cmd argument parsing to use argparse module with tests in test_models

--- tests/test_models.py
+++ tests/test_models.py
@@ -5,7 +5,9 @@ Test the model distribution files
 import json
 import os
 import random as rnd
+import sys
 
+from extra.io import parse_args
 from extra.model import ModelDistribution
 
 
@@ -38,3 +40,33 @@ def test_model_probability():
 
         prob = rnd.random()
         print(f'Probability: {prob}')
+
+
+def test_args():
+    print()
+
+    # Files
+    sys.argv = ['location', '--file', 'basic']
+    assert parse_args() == {'model': 'models/basic.mdl', 'tasks': None, 'servers': None, 'repeat': 0}
+    sys.argv = ['location', '-f', 'test']
+    assert parse_args() == {'model': 'models/test.mdl', 'tasks': None, 'servers': None, 'repeat': 0}
+
+    sys.argv = ['location', '--file', 'basic', '--tasks', '3']
+    assert parse_args() == {'model': 'models/basic.mdl', 'tasks': 3, 'servers': None, 'repeat': 0}
+    sys.argv = ['location', '--file', 'basic', '-t', '4']
+    assert parse_args() == {'model': 'models/basic.mdl', 'tasks': 4, 'servers': None, 'repeat': 0}
+
+    sys.argv = ['location', '--file', 'basic', '--servers', '5']
+    assert parse_args() == {'model': 'models/basic.mdl', 'tasks': None, 'servers': 5, 'repeat': 0}
+    sys.argv = ['location', '--file', 'basic', '-s', '6']
+    assert parse_args() == {'model': 'models/basic.mdl', 'tasks': None, 'servers': 6, 'repeat': 0}
+
+    sys.argv = ['location', '--file', 'basic', '--repeat', '7']
+    assert parse_args() == {'model': 'models/basic.mdl', 'tasks': None, 'servers': None, 'repeat': 7}
+    sys.argv = ['location', '--file', 'basic', '-r', '8']
+    assert parse_args() == {'model': 'models/basic.mdl', 'tasks': None, 'servers': None, 'repeat': 8}
+
+    sys.argv = ['location', '--file', 'basic', '--tasks', '1', '--servers', '2', '--repeat', '3']
+    assert parse_args() == {'model': 'models/basic.mdl', 'tasks': 1, 'servers': 2, 'repeat': 3}
+    sys.argv = ['location', '--file', 'basic', '-t', '1', '-s', '2', '-r', '3']
+    assert parse_args() == {'model': 'models/basic.mdl', 'tasks': 1, 'servers': 2, 'repeat': 3}
