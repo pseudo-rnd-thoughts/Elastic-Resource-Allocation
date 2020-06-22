b009c2d322ea0be5f06637e488a0d7989ced3c05
StringTheory <mark.m.towers@gmail.com>
2020-06-22 14:26:07 +0100

    Moved optimality testing from evaluation to tests module

--- tests/test_optimality.py
+++ tests/test_optimality.py
@@ -4,12 +4,18 @@ Tests the effectiveness of the optimality time limit for the social welfare of t
 
 from __future__ import annotations
 
+import json
 from typing import Sequence
 
+from auctions.decentralised_iterative_auction import optimal_decentralised_iterative_auction
+from branch_bound.branch_bound import branch_bound_algorithm
+from branch_bound.feasibility_allocations import fixed_feasible_allocation
 from core.core import reset_model
-from extra.io import load_args
+from core.fixed_task import FixedSumSpeeds, FixedTask
+from core.super_server import SuperServer
+from extra.io import parse_args
 from extra.pprint import print_model
-from extra.model import ModelDistribution
+from extra.model import ModelDistribution, results_filename
 from optimal.optimal import optimal_solver
 
 
@@ -36,8 +42,46 @@ def test_optimal_time_limit(model_dist: ModelDistribution,
             break
 
 
+def optimal_testing(model_dist: ModelDistribution, repeat: int, repeats: int = 20):
+    """
+    Evaluates the results using the optimality
+
+    :param model_dist: The model distribution
+    :param repeat: The repeat of the testing
+    :param repeats: The number of repeats
+    """
+    data = []
+    for _ in range(repeats):
+        tasks, servers = model_dist.generate()
+
+        results = {}
+        optimal_result = branch_bound_algorithm(tasks, servers)
+        results['optimal'] = optimal_result.store()
+        reset_model(tasks, servers)
+        relaxed_result = branch_bound_algorithm(tasks, [SuperServer(servers)])
+        results['relaxed'] = relaxed_result.store()
+        reset_model(tasks, servers)
+        fixed_tasks = [FixedTask(task, FixedSumSpeeds()) for task in tasks]
+        fixed_result = branch_bound_algorithm(fixed_tasks, servers, feasibility=fixed_feasible_allocation)
+        results['fixed'] = fixed_result.store()
+        reset_model(tasks, servers)
+
+        for price_change in [1, 2, 3, 5, 10]:
+            dia_result = optimal_decentralised_iterative_auction(tasks, servers)
+            results[f'dia {price_change}'] = dia_result.store()
+
+            reset_model(tasks, servers)
+
+        data.append(results)
+
+        # Save the results to the file
+        filename = results_filename('paper', model_dist, repeat)
+        with open(filename, 'w') as file:
+            json.dump(data, file)
+
+
 if __name__ == "__main__":
-    args = load_args()
+    args = parse_args()
     loaded_model_dist = ModelDistribution(args['model'], args['tasks'], args['servers'])
 
     test_optimal_time_limit(loaded_model_dist)
