644ec8b11d3d21bc53a19644ba58a461190cb607
StringTheory <mark.m.towers@gmail.com>
2020-06-24 14:47:54 +0100

    Fixed bash script with python path

--- tests/run_iridis_test.sh
+++ tests/run_iridis_test.sh
@@ -13,11 +13,7 @@ source activate env37
 # Load cplex
 module load cplex/12.7.1
 
-# Load Gurobi
-module load gurobi/8.1.1
-export GRB_LICENSE_FILE=/local/software/gurobi/8.1.1/gurobi.lic
-
 # Run the python script
-cmd="python -m tests.test_iridis"
+cmd="PYTHONPATH=~/Flexible-Cloud-Resource/src/ python tests/test_iridis"
 echo "Running $cmd"
 eval "$cmd"
