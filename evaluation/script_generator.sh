f5bc70bc7c70d49cb3375e049852257d371ebde2
StringTheory <mark.m.towers@gmail.com>
2020-06-24 15:12:50 +0100

    Fixed script_generator.sh due to extra $ when reading variable

--- evaluation/script_generator.sh
+++ evaluation/script_generator.sh
@@ -7,9 +7,10 @@ read -p 'Num of repeats: ' repeats
 tasks=()
 servers=()
 while true; do
-  read -p 'Num of tasks: ' $num_task
+  read -p 'Num of tasks: ' num_task
 
   if [ "$num_task" == "" ]; then
+    echo 'No more tasks and servers'
     break
   else
     read -p 'Num of servers: ' num_servers
@@ -24,6 +25,7 @@ read -p 'Extra info: ' extra
 for (( pos = 0; pos < ${#jobs[@]}; pos++ )); do
   for (( repeat = 0; repeat < repeats; repeat++ )); do
     cmd="qsub -v file='$script',model_file='$model_file',num_task='${tasks[pos]}',num_servers='${servers[pos]}',repeat='$repeat',extra='$extra' run_script.sh"
+    echo "$cmd"
     eval "$cmd"
   done
 done
