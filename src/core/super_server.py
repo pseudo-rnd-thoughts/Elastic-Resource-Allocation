607d50d97fb695a179a8312816fea44835291e36
StringTheory <mark.m.towers@gmail.com>
2020-07-06 19:44:18 +0100

    Selects the most common price change and initial price parameters

--- src/core/super_server.py
+++ src/core/super_server.py
@@ -14,7 +14,11 @@ class SuperServer(Server):
     """Super Server which is the power rangers megazord of servers"""
 
     def __init__(self, servers: List[Server]):
+        price_changes = [server.price_change for server in servers]
+        initial_prices = [server.initial_price for server in servers]
         Server.__init__(self, 'Super Server',
-                        sum(server.storage_capacity for server in servers),
-                        sum(server.computation_capacity for server in servers),
-                        sum(server.bandwidth_capacity for server in servers))
+                        storage_capacity=sum(server.storage_capacity for server in servers),
+                        computation_capacity=sum(server.computation_capacity for server in servers),
+                        bandwidth_capacity=sum(server.bandwidth_capacity for server in servers),
+                        price_change=max(set(price_changes), key=price_changes.count),
+                        initial_price=max(set(initial_prices), key=initial_prices.count))
