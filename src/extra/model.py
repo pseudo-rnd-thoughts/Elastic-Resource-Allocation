3dd65d1b2bc6752328fe66168a4495a6c3cd3efc
StringTheory <mark.m.towers@gmail.com>
2020-07-10 11:38:12 +0100

    Add generate_online in order to generate tasks according to the mean and std arrival rate of tasks over time.

--- src/extra/model.py
+++ src/extra/model.py
@@ -4,7 +4,6 @@ Model distribution
 
 from __future__ import annotations
 
-import datetime as dt
 import json
 import random as rnd
 from typing import TYPE_CHECKING
@@ -37,6 +36,25 @@ class ModelDistribution:
         """
         return self.generate_tasks(), self.generate_servers()
 
+    def generate_online(self, time_steps: int, mean_arrival_rate: int,
+                        std_arrival_rate: float) -> Tuple[List[Task], List[Server]]:
+        """
+        Create a list of tasks and servers from a task and server distribution with online distribution
+
+        :param time_steps: Number of time steps
+        :param mean_arrival_rate: Mean number of tasks that arrive each time steps
+        :param std_arrival_rate: Standard deviation of the number of tasks that arrive each time steps
+        :return: A list of tasks and list of servers
+        """
+        tasks, task_id = [], 0
+        for time_step in range(time_steps):
+            for _ in range(max(1, int(rnd.gauss(mean_arrival_rate, std_arrival_rate)))):
+                task = self.generate_rnd_task(task_id)
+                task.auction_time, task_id = time_step, task_id + 1
+                tasks.append(task)
+
+        return tasks, self.generate_servers()
+
     def generate_tasks(self) -> List[Task]:
         """
         Generate a list of tasks from the model data
@@ -44,20 +62,10 @@ class ModelDistribution:
         :return: A list of tasks
         """
         if 'task distributions' in self.model_data:
+            assert self.num_tasks is not None
             return [self.generate_rnd_task(task_id) for task_id in range(self.num_tasks)]
         else:
             return [Task.load(task_model) for task_model in self.model_data['tasks']]
-
-    def generate_servers(self) -> List[Server]:
-        """
-        Generate a list of server from the model data
-
-        :return: A list of servers
-        """
-        if 'server distributions' in self.model_data:
-            return [self.generate_rnd_server(server_id) for server_id in range(self.num_servers)]
-        else:
-            return [Server.load(server_model) for server_model in self.model_data['servers']]
 
     def generate_rnd_task(self, task_id) -> Task:
         """
@@ -71,6 +79,18 @@ class ModelDistribution:
                          if probability <= sum(self.model_data['task distributions'][j]['probability']
                                                for j in range(i + 1)))
         return Task.load_dist(task_dist, task_id)
+
+    def generate_servers(self) -> List[Server]:
+        """
+        Generate a list of server from the model data
+
+        :return: A list of servers
+        """
+        if 'server distributions' in self.model_data:
+            assert self.num_servers is not None
+            return [self.generate_rnd_server(server_id) for server_id in range(self.num_servers)]
+        else:
+            return [Server.load(server_model) for server_model in self.model_data['servers']]
 
     def generate_rnd_server(self, server_id) -> Server:
         """
