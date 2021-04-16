9b72334cc86baa363214223b0f504f8dad163498
StringTheory <mark.m.towers@gmail.com>
2021-04-16 10:47:43 +0100

    Change the 15 tasks to 3 servers to run the fixed optimal and not the full optimal script

--- evaluation/alibaba_cmds.sh
+++ evaluation/alibaba_cmds.sh
@@ -1,7 +1,7 @@
 qsub -v file='alibaba',model_file='alibaba',num_tasks='10',num_servers='10',extra='model sizing' run_script.sh
 
 qsub -v file='greedy',model_file='alibaba',num_tasks='10',num_servers='2',extra='full optimal' run_script.sh
-qsub -v file='greedy',model_file='alibaba',num_tasks='15',num_servers='3',extra='full optimal' run_script.sh
+qsub -v file='greedy',model_file='alibaba',num_tasks='15',num_servers='3',extra='fixed optimal' run_script.sh
 qsub -v file='greedy',model_file='alibaba',num_tasks='20',num_servers='4',extra='fixed optimal' run_script.sh
 qsub -v file='greedy',model_file='alibaba',num_tasks='30',num_servers='6',extra='fixed optimal' run_script.sh
 qsub -v file='greedy',model_file='alibaba',num_tasks='50',num_servers='10',extra='time limited' run_script.sh
