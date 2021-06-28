e6409e4386a709c85aee46c7d9d1f1041e629828
StringTheory <mark.m.towers@gmail.com>
2021-06-28 16:58:54 +0100

    Remove replicate function on model sizing

--- evaluation/alibaba.py
+++ evaluation/alibaba.py
@@ -3,82 +3,14 @@ Tests the realistic settings
 """
 
 import json
-from typing import List, Tuple
 
 from src.core.core import reset_model
 from src.core.non_elastic_task import generate_non_elastic_tasks
 from src.extra.io import parse_args, results_filename
 from src.extra.model import AlibabaModelDist
-from src.greedy.greedy import greedy_permutations, greedy_algorithm
-from src.greedy.resource_allocation import SumPercentage
-from src.greedy.server_selection import SumResources
-from src.greedy.task_priority import UtilityDeadlinePerResourcePriority
-from src.optimal.non_elastic_optimal import non_elastic_optimal
+from src.greedy.greedy import greedy_permutations
 from src.optimal.elastic_optimal import elastic_optimal
-
-
-def model_scaling(model_dist: AlibabaModelDist, repeat_num: int, scaling: List[Tuple[int, int, int]],
-                  repeats: int = 50):
-    """
-    Tests a range of possible model scaling
-
-    :param model_dist: Model distribution
-    :param repeat_num: The repeat number
-    :param scaling: List of possible scaling
-    :param repeats: The number of repeats
-    """
-    scaling_results = {}
-    filename = results_filename('model_scaling', model_dist, repeat_num)
-    greedy_params = (UtilityDeadlinePerResourcePriority(), SumResources(), SumPercentage())
-
-    for storage_scaling, computation_scaling, results_scaling in scaling:
-        print(f'Storage scaling: {storage_scaling}, computational scaling: {computation_scaling}, '
-              f'results data scaling: {results_scaling}')
-        model_dist.storage_scaling = storage_scaling
-        model_dist.computational_scaling = computation_scaling
-        model_dist.results_scaling = results_scaling
-
-        model_results = []
-        for _ in range(repeats):
-            # Generate the tasks
-            servers = [model_dist.generate_server(server_id) for server_id in range(model_dist.num_servers)]
-            foreknowledge_tasks, requested_tasks = model_dist.generate_foreknowledge_requested_tasks(
-                servers, model_dist.num_tasks)
-            fixed_foreknowledge_tasks = generate_non_elastic_tasks(foreknowledge_tasks)
-            fixed_requested_tasks = generate_non_elastic_tasks(requested_tasks)
-            algorithm_results = {
-                'model': {
-                    'foreknowledge tasks': [task.save() for task in foreknowledge_tasks],
-                    'fixed foreknowledge tasks': [task.save() for task in fixed_foreknowledge_tasks],
-                    'requested tasks': [task.save() for task in requested_tasks],
-                    'fixed requested task': [task.save() for task in fixed_requested_tasks],
-                    'servers': [server.save() for server in servers]
-                }
-            }
-
-            # Run the algorithms
-            results = non_elastic_optimal(fixed_foreknowledge_tasks, servers, time_limit=60)
-            algorithm_results['foreknowledge fixed optimal'] = results.store()
-            reset_model(fixed_foreknowledge_tasks, servers)
-
-            result = greedy_algorithm(foreknowledge_tasks, servers, *greedy_params)
-            algorithm_results['foreknowledge greedy'] = result.store()
-            reset_model(foreknowledge_tasks, servers)
-
-            results = non_elastic_optimal(fixed_requested_tasks, servers, time_limit=60)
-            algorithm_results['requested fixed optimal'] = results.store()
-            reset_model(fixed_requested_tasks, servers)
-
-            result = greedy_algorithm(requested_tasks, servers, *greedy_params)
-            algorithm_results['requested greedy'] = result.store()
-            reset_model(requested_tasks, servers)
-
-            model_results.append(algorithm_results)
-        scaling_results[f'{storage_scaling} {computation_scaling} {results_scaling}'] = model_results
-
-        # Save the results to the file
-        with open(filename, 'w') as file:
-            json.dump(scaling_results, file)
+from src.optimal.non_elastic_optimal import non_elastic_optimal
 
 
 def foreknowledge_evaluation(model_dist: AlibabaModelDist, repeat_num: int, repeats: int = 50,
