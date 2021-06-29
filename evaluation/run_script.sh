d96183df6a27c0a9de350a961c7284dacdc86ef9
StringTheory <mark.m.towers@gmail.com>
2021-06-29 11:26:04 +0100

    Location of run_script updated for new project name

--- evaluation/run_script.sh
+++ evaluation/run_script.sh
@@ -4,7 +4,7 @@
 #PBS -l nodes=1:ppn=8
 
 #Change to the directory that the job was submitted from
-cd ~/Flexible-Cloud-Resource/
+cd ~/Elastic-Resource-Allocation/
 
 # Load python 3.7
 module load conda/4.4.0
@@ -15,7 +15,7 @@ module load cplex/12.7.1
 
 # Run the python script
 echo $PWD
-PYTHONPATH=~/Flexible-Cloud-Resource/src/
+PYTHONPATH=~/Elastic-Resource-Allocation/src/
 cmd="python -m evaluation.$file -m='$model_file' -t='$num_tasks' -s='$num_servers' -r='$repeat' -e='$extra'"
 echo "Running $cmd"
 eval "$cmd"
