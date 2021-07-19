d8168be70414fe695d7c7dfe31566f05cbf713eb
StringTheory <mark.m.towers@gmail.com>
2021-07-19 15:23:01 +0100

    Update the list of cmds

--- evaluation/cmds_alibaba.sh
+++ evaluation/cmds_alibaba.sh
@@ -1,5 +1,6 @@
 qsub -v file='alibaba',model_file='alibaba',num_tasks='30',num_servers='6',extra='model scaling' run_script.sh
 qsub -v file='alibaba',model_file='alibaba',num_tasks='500',num_servers='6',extra='task sizing' run_script.sh
+qsub -v file='alibaba',model_file='alibaba',num_tasks='500',num_servers='6',extra='server sizing' run_script.sh
 
 qsub -v file='greedy',model_file='alibaba',num_tasks='10',num_servers='2',extra='elastic optimal' run_script.sh
 qsub -v file='greedy',model_file='alibaba',num_tasks='15',num_servers='3',extra='relaxed optimal' run_script.sh
@@ -19,3 +20,8 @@ qsub -v file='alibaba',model_file='alibaba',num_tasks='30',num_servers='6',extra
 qsub -v file='resource_ratio',model_file='alibaba',num_tasks='30',num_servers='6',extra='non-elastic optimal' run_script.sh
 qsub -v file='resource_ratio',model_file='alibaba',num_tasks='30',num_servers='6',extra='time limited' run_script.sh
 qsub -v file='resource_ratio',model_file='alibaba',num_tasks='20',num_servers='4',extra='non-elastic optimal' run_script.sh
+
+qsub -v file='online',model_file='alibaba',num_servers='4' run_script.sh
+qsub -v file='online',model_file='alibaba',num_servers='6' run_script.sh
+qsub -v file='online',model_file='alibaba',num_servers='4',extra='greedy' run_script.sh
+qsub -v file='online',model_file='alibaba',num_servers='6',extra='greedy' run_script.sh
--- evaluation/cmds_synthetic.sh
+++ evaluation/cmds_synthetic.sh
@@ -1,21 +1,18 @@
 qsub -v file='greedy',model_file='synthetic',num_tasks='10',num_servers='2',extra='elastic optimal' run_script.sh
 qsub -v file='greedy',model_file='synthetic',num_tasks='15',num_servers='3',extra='elastic optimal' run_script.sh
-qsub -v file='greedy',model_file='synthetic',num_tasks='15',num_servers='3',extra='relaxed optimal' run_script.sh
-qsub -v file='greedy',model_file='synthetic',num_tasks='15',num_servers='3',extra='non-elastic optimal' run_script.sh
 qsub -v file='greedy',model_file='synthetic',num_tasks='20',num_servers='4',extra='relaxed optimal' run_script.sh
 qsub -v file='greedy',model_file='synthetic',num_tasks='20',num_servers='4',extra='non-elastic optimal' run_script.sh
 qsub -v file='greedy',model_file='synthetic',num_tasks='30',num_servers='6',extra='non-elastic optimal' run_script.sh
-qsub -v file='greedy',model_file='synthetic',num_tasks='30',num_servers='6',extra='relaxed optimal' run_script.sh
 qsub -v file='greedy',model_file='synthetic',num_tasks='40',num_servers='8',extra='greedy' run_script.sh
-
 qsub -v file='greedy',model_file='synthetic',num_tasks='80',num_servers='16',extra='greedy' run_script.sh
 qsub -v file='greedy',model_file='synthetic',num_tasks='160',num_servers='32',extra='greedy' run_script.sh
 
 qsub -v file='greedy',model_file='synthetic',num_tasks='40',num_servers='8',extra='lower bound' run_script.sh
 
 qsub -v file='auctions',model_file='synthetic',num_tasks='10',num_servers='2',extra='elastic optimal' run_script.sh
-qsub -v file='auctions',model_file='synthetic',num_tasks='15',num_servers='3',extra='elastic optimal' run_script.sh
+# qsub -v file='auctions',model_file='synthetic',num_tasks='15',num_servers='3',extra='elastic optimal' run_script.sh
 qsub -v file='auctions',model_file='synthetic',num_tasks='15',num_servers='3',extra='non-elastic optimal' run_script.sh
+qsub -v file='auctions',model_file='synthetic',num_tasks='20',num_servers='4',extra='non-elastic optimal' run_script.sh
 qsub -v file='auctions',model_file='synthetic',num_tasks='30',num_servers='6',extra='non-elastic optimal' run_script.sh
 qsub -v file='auctions',model_file='synthetic',num_tasks='30',num_servers='6',extra='greedy' run_script.sh
 qsub -v file='auctions',model_file='synthetic',num_tasks='40',num_servers='8',extra='greedy' run_script.sh
