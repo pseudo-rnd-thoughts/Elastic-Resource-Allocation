93b71ae5d125e1fa01fe724635b36f3214e0a110
StringTheory <mark.m.towers@gmail.com>
2021-07-20 14:38:16 +0100

    Moved to Iridis 5 supercomputer at University of Southampton, therefore modified the run scripts

---
+++ evaluation/iridis/synthetic/greedy_t40_s8_greedy.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+        
+#SBATCH --time=60:00:00
+
+#Change to the directory that the job was submitted from
+cd ~/Elastic-Resource-Allocation/
+
+# Load cplex
+module load cplex/20.1.0
+module unload python/3.6.4
+
+# Load python 3.7
+module load conda
+source activate Elastic-Resource-Allocation
+
+# Run the python script
+echo $PWD
+PYTHONPATH=~/Elastic-Resource-Allocation/src/
+cmd="python -m evaluation.greedy -m='synthetic' -t='40' -s='8' -r='0' -e='greedy'"
+echo "Running $cmd"
+eval "$cmd"
---
+++ evaluation/iridis/synthetic/mutation_t30_s6_special case.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+        
+#SBATCH --time=60:00:00
+
+#Change to the directory that the job was submitted from
+cd ~/Elastic-Resource-Allocation/
+
+# Load cplex
+module load cplex/20.1.0
+module unload python/3.6.4
+
+# Load python 3.7
+module load conda
+source activate Elastic-Resource-Allocation
+
+# Run the python script
+echo $PWD
+PYTHONPATH=~/Elastic-Resource-Allocation/src/
+cmd="python -m evaluation.mutation -m='synthetic' -t='30' -s='6' -r='0' -e='special case'"
+echo "Running $cmd"
+eval "$cmd"
