66dc5ca90f026e6ed94e36363842bce97757f1aa
StringTheory <mark.m.towers@gmail.com>
2021-07-26 14:37:08 +0100

    Added a model size testing for the greedy where a range of model sizes to tested at the same time

--- evaluation/greedy.py
+++ evaluation/greedy.py
@@ -11,9 +11,10 @@ from src.core.core import reset_model
 from src.extra.io import parse_args, results_filename
 from src.extra.model import ModelDist, get_model, generate_evaluation_model
 from src.greedy.greedy import greedy_algorithm, greedy_permutations
-from src.greedy.resource_allocation import resource_allocation_functions
-from src.greedy.server_selection import server_selection_functions
-from src.greedy.task_priority import task_priority_functions, ValuePriority
+from src.greedy.resource_allocation import resource_allocation_functions, SumPowPercentage
+from src.greedy.server_selection import server_selection_functions, ProductResources
+from src.greedy.task_priority import task_priority_functions, ValuePriority, UtilityDeadlinePerResourcePriority, \
+    ResourceSumPriority
 from src.optimal.non_elastic_optimal import non_elastic_optimal
 from src.optimal.elastic_optimal import elastic_optimal, server_relaxed_elastic_optimal
 
@@ -122,15 +123,17 @@ def algorithm_sizes(model_dist: ModelDist, repeat_num: int, repeats: int = 30):
     pretty_printer, scale_results = PrettyPrinter(), {}
     filename = results_filename('greedy_model_sizes', model_dist, repeat_num)
     for num_tasks, num_servers in ((10, 2), (15, 3), (20, 4), (30, 6), (40, 8), (80, 16), (160, 32)):
+        print(f'Numbers of tasks: {num_tasks}, number of servers: {num_servers}')
         model_dist.num_tasks = num_tasks
         model_dist.num_servers = num_servers
 
         model_results = []
         for _ in range(repeats):
             tasks, servers = model_dist.generate_oneshot()
-            algorithm_results = {}
-            greedy_permutations(tasks, servers, algorithm_results)
-            model_results.append(algorithm_results)
+            results = greedy_algorithm(tasks, servers,
+                                       task_priority=UtilityDeadlinePerResourcePriority(ResourceSumPriority()),
+                                       server_selection=ProductResources(), resource_allocation=SumPowPercentage())
+            model_results.append(results.store())
 
         scale_results[f'{num_tasks} tasks, {num_servers} servers'] = model_results
 
